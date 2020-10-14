55f7a197412f30172e5d8b2cb6598b7bebb43b00
KatyaPanukova <panyukowa-yekaterina@yandex.ru>
2020-10-14 10:28:44 +0300

    done

--- project.py
+++ project.py
@@ -6,12 +6,20 @@ class Main:
 
     def __getitem__(self, item):
         """Get item method"""
+        try:
+            return self.list_[item - 1]
+        except IndexError:
+            print('Index out of range!')
 
     def __setitem__(self, key, value):
         """Set item method"""
+        try:
+            self.list_[key-1] = value
+        except IndexError:
+            print('Key out of range!')
 
     def __str__(self):
         """String method"""
-        return f''
+        return f'{self.list_}'
 
 
