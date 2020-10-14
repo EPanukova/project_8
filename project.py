8e5abe5ff65c219494ab38e0a8a9ca8113c1a937
KatyaPanukova <panyukowa-yekaterina@yandex.ru>
2020-10-14 10:13:08 +0300

    start

---
+++ project.py
@@ -0,0 +1,17 @@
+class Main:
+    """Class recalculate index"""
+    def __init__(self, list_):
+        """Initialisation method"""
+        self.list_ = list_
+
+    def __getitem__(self, item):
+        """Get item method"""
+
+    def __setitem__(self, key, value):
+        """Set item method"""
+
+    def __str__(self):
+        """String method"""
+        return f''
+
+
